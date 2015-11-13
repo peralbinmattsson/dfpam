06d37e8cb2ebfe335ecedca5097554af4964ac71
PAM <peralbin@ponty.se>
2015-11-13 10:42:10 +0100

    Copied dotfiles.

---
+++ README.md
@@ -0,0 +1,13 @@
+# dotfiles
+This repo contains some dotfiles to ease the process of having a known
+environment wherever I go.
+
+Most of the stuff is "stolen"/inspired from others. What made me choose this
+setup was looking at [Mitsuhiko's](https://github.com/mitsuhiko) dotfiles
+repo.
+
+My Vim plugins is handled with Vundle. Read more about it 
+[here](http://www.charlietanksley.net/philtex/sane-vim-plugin-management/).
+
+## Note to self
+- Install [vcprompt](https://bitbucket.org/mitsuhiko/vcprompt)
---
+++ vim/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash                                                                     
+                                                                                
+echo "Installing..."                                                            
+                                                                                
+# Do vundle install, should be handled from within vimrc
+vim +q                                             
+																										  
+ln -sf ~/.vim/bundle/xmledit/ftplugin/xml.vim ~/.vim/bundle/xmledit/ftplugin/html.vim
+# Make xmledit autocomplete jinja files
+if [ -f ~/.vim/bundle/xmledit/ftplugin/jinja.vim ]
+then
+  echo "jinja.vim symlink already exists"
+else
+  ln -s ~/.vim/bundle/xmledit/ftplugin/html.vim ~/.vim/bundle/xmledit/ftplugin/jinja.vim
+fi
+
+#if [ ! -f ~/.vim/bundle/command-t/ruby/command-t/ext.so ]
+#then
+#	cd ~/.vim/bundle/command-t/ruby/command-t
+#	ruby extconf.rb
+#	make
+#fi
+																										  
+echo "Done." 
